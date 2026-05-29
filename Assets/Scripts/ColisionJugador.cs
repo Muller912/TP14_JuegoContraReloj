73bec08c79343a4a0a3ce483afcdeafbd36b0563
Alex <mulleralex204@gmail.com>
2026-05-29 10:02:47 -0300

    14.2, COLISION + "UIMANAGER"

---
+++ Assets/Scripts/ColisionJugador.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColisionJugador : MonoBehaviour
+
+{
+    public int hamburguesatotal = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider col){
+          if(col.gameObject.CompareTag("Hamburguesa")){
+            Destroy(col.gameObject);
+            hamburguesatotal = hamburguesatotal + 1;
+    }
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+
+{
+    public ColisionJugador ColisionJugador;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(ColisionJugador.hamburguesatotal);
+    }
+}
