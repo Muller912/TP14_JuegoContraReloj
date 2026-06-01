a1393d2346a75806bcde9c1c4a56c6ab6d0c99f5
Alex <mulleralex204@gmail.com>
2026-06-01 12:09:06 -0300

    CASI TERMINADO 4

---
+++ Assets/Scripts/RotarEnEje.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotarEnEje : MonoBehaviour
+{
+    public float velocidad = 50f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, velocidad * Time.deltaTime, 0);
+    }
+}
